8559decd911349c1f6e3b8fc19d8e00f61a777ad
Yusuf Dede <yusufdede95@hotmail.com>
2020-05-31 02:48:02 +0300

    finish test project & refactor time!

--- Controllers/ParkingController.cs
+++ Controllers/ParkingController.cs
@@ -39,42 +39,83 @@ namespace parking_automation_dotnet_core.Controllers
                     trusted = true;
                 }
 
-                Parking cars = new Parking()
+                if (Parking.carsCount() < 5)
                 {
-                    ID = id,
-                    Name = name,
-                    Color = color,
-                    Plaque = plaque,
-                    EnterDate = DateTime.Now,
-                    Trusted = Convert.ToBoolean(trusted)
-                };
+                    Parking cars = new Parking()
+                    {
+                        ID = id,
+                        Name = name,
+                        Color = color,
+                        Plaque = plaque,
+                        EnterDate = DateTime.Now,
+                        Trusted = Convert.ToBoolean(trusted)
+                    };
 
-                var DummyDataFile = Parking.DummyDataFile;
-                var DummyData = System.IO.File.ReadAllText(DummyDataFile);
-                List<Parking> CarList = new List<Parking>();
-                CarList = JsonConvert.DeserializeObject<List<Parking>>(DummyData);
+                    var DummyDataFile = Parking.DummyDataFile;
+                    var DummyData = System.IO.File.ReadAllText(DummyDataFile);
+                    List<Parking> CarList = new List<Parking>();
+                    CarList = JsonConvert.DeserializeObject<List<Parking>>(DummyData);
 
-                if (CarList == null)
+                    if (CarList == null)
+                    {
+                        CarList = new List<Parking>();
+                    }
+
+                    CarList.Add(cars);
+
+                    System.IO.File.WriteAllText(DummyDataFile, JsonConvert.SerializeObject(CarList));
+
+                    created = true;
+                }
+                else
                 {
-                    CarList = new List<Parking>();
+                    ViewBag.Warning = "AutoPark Full | Not car add";
                 }
 
-                CarList.Add(cars);
-
-                System.IO.File.WriteAllText(DummyDataFile, JsonConvert.SerializeObject(CarList));
-
-                created = true;
             }
 
             if (created)
             {
                 ViewBag.Message = "Car was created succesfully!...";
+                return RedirectToAction("Index", "Parking");
             }
             else
             {
                 ViewBag.Message = "There was an error while creating the car.";
             }
+
             return View();
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var Cars = Parking.GetCars();
+            var deleted = false;
+
+            foreach (Parking car in Cars)
+            {
+                if (car.ID == id)
+                {
+                    var index = Cars.IndexOf(car);
+                    Cars.RemoveAt(index);
+
+                    System.IO.File.WriteAllText(Parking.DummyDataFile, JsonConvert.SerializeObject(Cars));
+                    deleted = true;
+                    break;
+                }
+            }
+
+            if (deleted)
+            {
+                ViewBag.Message = "Car was deleted successfull.";
+            }
+            else
+            {
+                ViewBag.Message = "There was an error while deleting the car.";
+            }
+
+            return RedirectToAction("Index", "Parking");
         }
     }
 }
